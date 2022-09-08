39d8fa05dc7ee5f2ebe02122a579db9fd5cf9136
Roman Stratiienko <r.stratiienko@gmail.com>
2022-09-08 18:41:09 +0300

    minigbm: Add drv_bo_get_pixel_stride function
    Some drivers may copy/convert the buffer during mapping and
    in some cases stride of copied image can be different from
    original. Android uses pixel_stride for CPU access and need
    map_time stride instead of original stride in this cases.
    
    Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>

--- drv_priv.h
+++ drv_priv.h
@@ -100,6 +100,7 @@ struct backend {
 	int (*bo_unmap)(struct bo *bo, struct vma *vma);
 	int (*bo_invalidate)(struct bo *bo, struct mapping *mapping);
 	int (*bo_flush)(struct bo *bo, struct mapping *mapping);
+	uint32_t (*bo_get_map_stride)(struct bo *bo);
 	void (*resolve_format_and_use_flags)(struct driver *drv, uint32_t format,
 					     uint64_t use_flags, uint32_t *out_format,
 					     uint64_t *out_use_flags);
